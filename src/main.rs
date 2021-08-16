334dd56fce1285571c2391c04d16fa442d32b145
Loakesh I <loakesh.indiran@wowlabz.com>
2021-08-16 07:23:01 +0530

    Add: Actix Server with home route and catchers Implemented:
    Because:

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+mod controller;
+mod models;
+mod services;
+
+use rocket::{http::Status, response::status};
+use serde_json::{json, Value};
+
+#[get("/")]
+fn api_home() -> status::Custom<Value> {
+    let message = json!({"success": true, "data": "Authentication Server"});
+    status::Custom(Status::Ok, message)
+}
+
+#[catch(404)]
+fn not_found() -> status::Custom<Value> {
+    let message = json!({ "success": false, "data": "Not found!" });
+    status::Custom(Status::NotFound, message)
+}
+
+#[launch]
+async fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![api_home])
+        .register("/", catchers![not_found])
 }
